7f33f587ab6b5ece48783d5e9f12bc79eae20536
johnptmcdonald <johnptmcdonald@gmail.com>
2016-05-16 21:47:20 -0400

    adds facebook auth and readme reflections

--- config/auth.js
+++ config/auth.js
@@ -1,8 +1,8 @@
 module.exports = {
 
     'facebookAuth' : {
-        'clientID'      : 'your-secret-clientID-here', 
-        'clientSecret'  : 'your-client-secret-here', 
+        'clientID'      : 'your-secret-clientID-here', // your App ID
+        'clientSecret'  : 'your-client-secret-here', // your App Secret
         'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
     },
 
